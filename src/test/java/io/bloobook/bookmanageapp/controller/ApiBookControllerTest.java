0459989c5e1fdc018c384f2187c8ce9af70ba106
DevBloo <devchanghwan@gmail.com>
2021-05-10 23:33:23 +0900

    test: BookController saveBook test 구현

--- src/test/java/io/bloobook/bookmanageapp/controller/ApiBookControllerTest.java
+++ src/test/java/io/bloobook/bookmanageapp/controller/ApiBookControllerTest.java
@@ -1,7 +1,81 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.bloobook.bookmanageapp.controller;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.bloobook.bookmanageapp.common.dto.request.BookSaveRequest;
+import io.bloobook.bookmanageapp.docs.BookDocumentation;
+import io.bloobook.bookmanageapp.service.ApiBookService;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
 /**
- *  @CreateBy: Bloo 
- *  @Date: 2021/05/10
- */class ApiBookControllerTest {
-  
+ * @CreateBy: Bloo
+ * @Date: 2021/05/10
+ */
+
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+@WebMvcTest (ApiBookController.class)
+class ApiBookControllerTest {
+
+    @MockBean
+    private ApiBookService bookService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp ( WebApplicationContext webApplicationContext, RestDocumentationContextProvider provider ) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilter(new CharacterEncodingFilter("UTF-8"))
+            .apply(documentationConfiguration(provider))
+            .alwaysDo(print())
+            .build();
+    }
+
+    @DisplayName ("신규 도서 등록을 테스트 한다.")
+    @Test
+    void saveNewBook () throws Exception {
+        // given
+        BookSaveRequest bookSaveRequest = BookSaveRequest.builder()
+            .categoryId(1L)
+            .locationCode("A열 2층 선반")
+            .bookCode("A-230-394")
+            .title("King Of Lear")
+            .bookIntroduction("도서 소개글 도서 소개글 입니다.")
+            .author("김작가")
+            .thumbnail("www.google.com")
+            .publisherBusinessNumber("2309-A3933-2222")
+            .publicationAt(LocalDate.of(2015, 4, 23))
+            .build();
+
+        // when
+        mockMvc.perform(post("/api/books")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(bookSaveRequest))
+        )
+            .andExpect(status().isCreated())
+            .andDo(BookDocumentation.saveNewBook());
+    }
 }
--- src/test/java/io/bloobook/bookmanageapp/docs/BookDocumentation.java
+++ src/test/java/io/bloobook/bookmanageapp/docs/BookDocumentation.java
@@ -1,5 +1,36 @@
-package io.bloobook.bookmanageapp.docs;/**
- *  @CreateBy: Bloo 
- *  @Date: 2021/05/10
- */public class BookDocumentation {
+package io.bloobook.bookmanageapp.docs;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultHandler;
+
+/**
+ * @CreateBy: Bloo
+ * @Date: 2021/05/10
+ */
+public class BookDocumentation {
+
+    public static ResultHandler saveNewBook () {
+        return document("books/save",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("categoryId").type(JsonFieldType.NUMBER).description("조회할 도서 ID"),
+                fieldWithPath("locationCode").type(JsonFieldType.STRING).description("도서 위치"),
+                fieldWithPath("bookCode").type(JsonFieldType.STRING).description("도서 등록 코드"),
+                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                fieldWithPath("bookIntroduction").type(JsonFieldType.STRING).description("소개글"),
+                fieldWithPath("author").type(JsonFieldType.STRING).description("작가명"),
+                fieldWithPath("thumbnail").type(JsonFieldType.STRING).description("썸네일 URL"),
+                fieldWithPath("publisherBusinessNumber").type(JsonFieldType.STRING).description("출판사 사업자 번호"),
+                fieldWithPath("publicationAt").type(JsonFieldType.STRING).description("도서 설명")
+            )
+        );
+    }
 }
